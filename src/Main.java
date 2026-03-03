2580ed0a15aa25ba5722a2465daae0bb000e3cbc
Suncncy <3378035936@qq.com>
2026-03-03 17:04:39 +0800

    静态变量和静态方法

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ public class Main{
     public static void main(String[] args){
 Persion p=new Persion("小红",20,"女");
 Persion p2=new Persion();
+Persion p3=new Persion();
 p.hello();
 p2.hello();
+//p3.info="Hello world";//给静态变量赋值
+        Persion.info=("直接赋值");//直接给静态变量赋值
+//System.out.println(p3.info);
+System.out.println(Persion.info);
+Persion.test();//打印静态函数里面的内容
     }
 }
--- src/Persion.java
+++ src/Persion.java
@@ -2,6 +2,7 @@ public class Persion {
     String name;
     int age;
     String gender;
+    static String info;//定义了一个静态变量
     Persion(){};
     Persion(String name,int age){
         this(name,age,null);//利用下面这个完整的函数及逆行构造
@@ -17,6 +18,9 @@ Persion(String name,int age,String gender){//这是个构造方法
 void hello(){
 
     System.out.println("我是"+name+"年龄"+age+"性别"+gender);
+}
+ static void test(){//静态变量函数，直接和类挂钩，跟成员里面的属性没有一点关系，也和类里面的函数没有关系，可以获取静态变量
+        System.out.println("Hello world");
 }
 
 }
