b1d44f1b5294bdeaa6aae9d8bab7d76f2a8fa041
Suncncy <3378035936@qq.com>
2026-03-05 20:11:00 +0800

    封装

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,7 +1,43 @@
 package entity;
 
 public class Person {
-    String name;
-    int age;
-    String gender;
+    private String name;
+    private int age;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.contains("小")){
+            return ;
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    private static Person instance;
+    private Person(){
+    }//加入这一行相当于把上面的pulic的Person代替掉了
+    public static Person getInstance(){
+       if(instance==null){
+           instance=new Person();
+       }
+       return instance;
+    }
 }
