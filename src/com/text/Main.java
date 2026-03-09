d6ad43c191bb7b093e99e9beb3ca64cb5fa678f5
Suncncy <3378035936@qq.com>
2026-03-09 21:23:42 +0800

    基本包装类，和特殊包装类

--- src/com/text/Main.java
+++ src/com/text/Main.java
@@ -3,16 +3,30 @@ package com.text;
 //import entity.Person;//进行导入设置，将person改成*号，就是相当于导入entity的所有类
 import entity.*;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+import static java.math.RoundingMode.CEILING;
+
 public class Main{
     public static void main(String[] args)  {
- Order order1=new Order(1,"珍珠奶茶","南阳理工学院");
- Order order2=new Order(2,"草莓冰激凌","南阳理工学院");
- System.out.println(order1);
- System.out.println(order1.equals(order2));//这个比较的变量里面的内容
-        System.out.println(order1.equals(order2));
+ //Integer i=Integer.valueOf(10);
+ //Integer i=new Integer(10);
+Integer i=666;//自动进行装箱机制,也可以自动进行拆箱，但是只限于基本数据类型
+        Integer a=666;//等于的值一定得大于-128，小于127就会返回缓存
+        Integer b=Integer.valueOf("6666666",16);//radix是进行进制转换的
+        Integer c=Integer.decode("0xA1");//这个也是进行进制转换
+            BigDecimal e=BigDecimal.valueOf(10);
 
+            System.out.println(e.divide(BigDecimal.valueOf(3),100, RoundingMode.CEILING));//进行小数点后面的精确，可以确定精确的位数，也可以确定精确的方式
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a == i);
+        BigInteger d=BigInteger.valueOf(Long.MAX_VALUE);
+        BigInteger result=d.multiply(d);
+        System.out.println(result);
+       // System.out.println(d.pow(100));这个是次方
+        System.out.println(d.bitLength());//计算占的位数
     }
 }
-//密封类。标准的声明格式：public sealed【abstract】【class/inerface】 类名【extend 父类】【implements 接口，......】permits【子类，.....】{
-//注意子类必须里面加final，sealed，或者no-sealed里面三个里面的一个
-//eg：public sealed class A permits B(是指定B继承A）;
