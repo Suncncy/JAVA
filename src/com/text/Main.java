64e4407929e42bcbe8e95e953feab66f7a914c04
Suncncy <3378035936@qq.com>
2026-03-11 21:13:49 +0800

    一维数组和二维数组

--- src/com/text/Main.java
+++ src/com/text/Main.java
@@ -11,22 +11,28 @@ import static java.math.RoundingMode.CEILING;
 
 public class Main{
     public static void main(String[] args)  {
- //Integer i=Integer.valueOf(10);
- //Integer i=new Integer(10);
-Integer i=666;//自动进行装箱机制,也可以自动进行拆箱，但是只限于基本数据类型
-        Integer a=666;//等于的值一定得大于-128，小于127就会返回缓存
-        Integer b=Integer.valueOf("6666666",16);//radix是进行进制转换的
-        Integer c=Integer.decode("0xA1");//这个也是进行进制转换
-            BigDecimal e=BigDecimal.valueOf(10);
+// int [] arr=new int[10];//创建一个对象
+// int [] arr2=new int[]{25,26,24};//直接进行赋值
+            int [] arr2={25,26,24};//后续不能再次进行更改
+//        int[][]arr3=new int [2][10];
+//        arr3[0][2]=23;
+        int [][] arr3={{1,3},{5,6},{9,10}};
+        for (int[] ints : arr3) {
+            for (int anInt : ints) {
+                System.out.println(anInt);
+            }
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!");
+        }//进行二维元素的分组遍历
+        System.out.println(arr3[0][1]);
+        System.out.println("--------------------");
+        arr2[0]=2;
+        arr2[1]=5;
+        System.out.println(arr2[0]);
+        System.out.println(arr2.toString());
+        for(int element:arr2){
+            System.out.println(element);
+        }//相当于直接进行遍历了
 
-            System.out.println(e.divide(BigDecimal.valueOf(3),100, RoundingMode.CEILING));//进行小数点后面的精确，可以确定精确的位数，也可以确定精确的方式
-        System.out.println(c);
-        System.out.println(b);
-        System.out.println(a == i);
-        BigInteger d=BigInteger.valueOf(Long.MAX_VALUE);
-        BigInteger result=d.multiply(d);
-        System.out.println(result);
-       // System.out.println(d.pow(100));这个是次方
-        System.out.println(d.bitLength());//计算占的位数
+
     }
 }
